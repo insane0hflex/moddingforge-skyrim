d53d166fa7d8be54e183b186c323a6262e45ce1a
insane0hflex <insane0hflex@users.noreply.github.com>
2016-06-16 18:30:06 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// write last updated to the lastUpdated class (in the footer tag at bottom of page)
-// Subs
+// write last modified date to the lastUpdated class (in the footer tag at bottom of page)
 $(document).ready(function () {
     $(".lastUpdated").text("Last Updated: " + document.lastModified.substring(0, 10));
 });
